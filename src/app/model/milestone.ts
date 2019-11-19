8989636a7b152274c3de9dc6d05f6deaffb64278
khuongnguyenkyanon <khuong.nguyen@kyanon.digital>
2019-11-20 03:44:04 +0700

    Add Model

---
+++ src/app/model/milestone.ts
@@ -0,0 +1,12 @@
+export class MileStone {
+  public time: string;
+  public company: string;
+  public projects?: Array<Project>;
+}
+
+export class Project {
+  public name: string;
+  public role: string;
+  public technologies?: Array<string>;
+  public description: string;
+}
